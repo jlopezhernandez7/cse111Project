0aedc57e1dfdf7d3b8f7695f711a252d229cc571
jlopezhernandez7 <joselopezhernandez1010@gmail.com>
2025-12-08 00:33:50 -0800

    commented out vercel.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 function App() {
   // ---------- auth state ---
   const [user, setUser] = useState(null);
